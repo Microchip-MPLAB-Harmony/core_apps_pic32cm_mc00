a3c06187362540ef6fadf17b82a70f4741256a48
Vishal Gandhi <vishal.gandhi@microchip.com>
2023-05-11 11:29:59 +0530

    [RTOS] Regenerated demo for rtos_freertos_basic_freertos_pic32cm_mc00_curiosity_pro
    MH3-75929

--- apps/rtos/freertos/basic_freertos/firmware/src/config/pic32cm_mc00_curiosity_pro/initialization.c
+++ apps/rtos/freertos/basic_freertos/firmware/src/config/pic32cm_mc00_curiosity_pro/initialization.c
@@ -55,7 +55,7 @@
 // ****************************************************************************
 // ****************************************************************************
 #pragma config NVMCTRL_BOOTPROT = SIZE_0BYTES
-#pragma config BODVDDUSERLEVEL = 0x0
+#pragma config BODVDDUSERLEVEL = 0x0U
 #pragma config BODVDD_DIS = ENABLED
 #pragma config BODVDD_ACTION = NONE
 #pragma config WDT_ENABLE = DISABLED
@@ -65,7 +65,7 @@
 #pragma config WDT_EWOFFSET = CYC8
 #pragma config WDT_WEN = DISABLED
 #pragma config BODVDD_HYST = DISABLED
-#pragma config NVMCTRL_REGION_LOCKS = 0xffff
+#pragma config NVMCTRL_REGION_LOCKS = 0xffffU
 
 
 
@@ -75,6 +75,11 @@
 // Section: Driver Initialization Data
 // *****************************************************************************
 // *****************************************************************************
+/* Following MISRA-C rules are deviated in the below code block */
+/* MISRA C-2012 Rule 11.1 */
+/* MISRA C-2012 Rule 11.3 */
+/* MISRA C-2012 Rule 11.8 */
+
 
 
 // *****************************************************************************
@@ -104,7 +109,7 @@
 // *****************************************************************************
 // *****************************************************************************
 
-
+/* MISRAC 2012 deviation block end */
 
 /*******************************************************************************
   Function:
@@ -118,8 +123,10 @@
 
 void SYS_Initialize ( void* data )
 {
+    /* MISRAC 2012 deviation block start */
+    /* MISRA C-2012 Rule 2.2 deviated in this file.  Deviation record ID -  H3_MISRAC_2012_R_2_2_DR_1 */
 
-    NVMCTRL_REGS->NVMCTRL_CTRLB = NVMCTRL_CTRLB_RWS(3);
+    NVMCTRL_REGS->NVMCTRL_CTRLB = NVMCTRL_CTRLB_RWS(3U);
 
   
     PORT_Initialize();
@@ -131,17 +138,24 @@ void SYS_Initialize ( void* data )
 
     NVMCTRL_Initialize( );
 
-    EVSYS_Initialize();
-
     SERCOM0_USART_Initialize();
+
+    EVSYS_Initialize();
 
     EIC_Initialize();
 
 	BSP_Initialize();
 
 
+    /* MISRAC 2012 deviation block start */
+    /* Following MISRA-C rules deviated in this block  */
+    /* MISRA C-2012 Rule 11.3 - Deviation record ID - H3_MISRAC_2012_R_11_3_DR_1 */
+    /* MISRA C-2012 Rule 11.8 - Deviation record ID - H3_MISRAC_2012_R_11_8_DR_1 */
 
 
+
+
+    /* MISRAC 2012 deviation block end */
     TASK1_Initialize();
     TASK2_Initialize();
     TASK3_Initialize();
@@ -149,6 +163,9 @@ void SYS_Initialize ( void* data )
 
 
     NVIC_Initialize();
+
+
+    /* MISRAC 2012 deviation block end */
 
 }
 
